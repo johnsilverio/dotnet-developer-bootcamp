77715c91e27a02886884b8a0d5c9954bcce1a120
johnsilverio <johnvitor2002@hotmail.com>
2024-05-13 12:40:49 -0300

    Aula sobre classes objetos e construtures em C#

---
+++ classes-objetos-construtures/ExemploExplorando/Program.cs
@@ -0,0 +1,23 @@
+﻿using ExemploExplorando.Models;
+
+// Usando contrutuor
+Pessoa p1 = new Pessoa(nome: "John", sobrenome: "Silverio");
+// Sem usar construtor
+Pessoa p2 = new Pessoa();
+p2.Nome = "Duda";
+p2.Sobrenome = "Queiroz";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAlunos(p1);
+cursoDeIngles.AdicionarAlunos(p2);
+cursoDeIngles.ListarAlunos();
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "John";
+// p1.Sobrenome = "Silverio";
+// p1.Idade = 22;
+// p1.Apresentar();
