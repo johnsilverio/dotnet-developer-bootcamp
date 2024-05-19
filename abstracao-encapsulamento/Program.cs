a541d61736ba4fd8e3898f24ca70f9ed768e16ef
johnsilverio <johnvitor2002@hotmail.com>
2024-05-19 18:50:12 -0300

    feat: Introdução a programação orientada a objetos, abstração e encapsulamento.

---
+++ abstracao-encapsulamento/Program.cs
@@ -0,0 +1,15 @@
+﻿using  abstracao_encapsulamento.Models;
+
+// Criando objeto da classe Pessoa
+Pessoa p1 = new Pessoa();
+p1.Nome = "John";
+p1.Idade = 20;
+
+p1.Apresentar(); 
+
+// Usando encapsulamento
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(5000);
+c1.ExibirSaldo();
